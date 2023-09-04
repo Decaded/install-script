726d49231cf49c76fdb29865360785d68aeaa526
Decaded <me@decaded.dev>
2023-09-04 05:40:08 +0200

    Prompt for net-tools install while conf static ip

--- install.sh
+++ install.sh
@@ -548,15 +548,13 @@ configure_static_ip() {
     fi
   fi
 
-  # Check if ifconfig is available, and if not, prompt the user to install it
-  if ! command -v ifconfig &>/dev/null; then
-    read -rp "ifconfig is not installed. Do you want to install it? (Y/n): " install_ifconfig
-    if [[ "$install_ifconfig" =~ ^[Yy]$ ]]; then
-      sudo apt install net-tools -y
-    else
-      echo "ifconfig is required to list network devices. Exiting."
-      exit 1
-    fi
+  # Prompt installation of ifconfig
+  read -rp "'ifconfig' (net-tools) package is required to proceed. Do you want to install it? (Y/n): " install_ifconfig
+  if [[ "$install_ifconfig" =~ ^[Yy]$ ]]; then
+    sudo apt install net-tools -y
+  else
+    echo "'ifconfig' (net-tools) is required to list network devices. Exiting."
+    exit 1
   fi
 
   # Get network device information from 'ifconfig -a'
